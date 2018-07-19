d635ac941d808b43a7126bd919adcd9421a1636f
MarkW <mark@liftedlogic.com>
2018-07-19 16:13:34 -0500

    WIP Location Map

--- components/location-map/location-map.js
+++ components/location-map/location-map.js
@@ -8,7 +8,10 @@
 
   var COMPONENT = {
 
-    className: 'll-location-map',
+
+    className: 'll-location-hero-banner',
+    mapPinIcon: 'M12 11.484q1.031 0 1.758-0.727t0.727-1.758-0.727-1.758-1.758-0.727-1.758 0.727-0.727 1.758 0.727 1.758 1.758 0.727zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z',
+    pinColor: '#2A77F0',
 
 
     selector : function() {
@@ -20,6 +23,142 @@
     init: function() {
 
       var _this = this;
+      $('.ll-location-map .location-map__googlemap').each( function(index, el) {
+
+        var map;
+        var locations = $(el).data('locations');
+        var bounds = new google.maps.LatLngBounds();
+        var mapId = $(el).attr('id');
+
+        function initialize_map() {
+          var center_coordinates = new google.maps.LatLng('43.0299189', '-77.505776');
+          var mapOptions = {
+            zoom: 11,
+            scrollwheel: false,
+            draggable: false,
+            disableDefaultUI: true,
+            maxZoom: 11,
+            center: center_coordinates,
+            styles: [
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#444444"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#edf3f9"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 45
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#46bcec"
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                }
+            ]
+          };
+
+          map = new google.maps.Map(document.getElementById( mapId ), mapOptions);
+//console.log(locations);
+          $.each( locations, function() {
+            var newMarker = this;
+            var latLng = {lat: parseFloat( newMarker['lat'] ), lng: parseFloat( newMarker['lng'] )};
+            //console.log( newMarker );
+
+            var icon = {
+              path: google.maps.SymbolPath.CIRCLE,
+              fillColor: _this.pinColor,
+              fillOpacity: 1,
+              anchor: new google.maps.Point( 12, 16 ),
+              strokeWeight: 0,
+              scale: 8
+            };
+
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map,
+              icon: icon
+            });
+
+            bounds.extend(marker.position);
+
+          });
+
+          map.fitBounds(bounds);
+
+        }
+
+        if ( typeof $(el) !== 'undefined' && $( el ).length > 0 ) {
+          google.maps.event.addDomListener(window, 'load', initialize_map);
+        }
+        /* Dev Note: this was here a second time. Seems like it should be nested in an "else"?
+        //initialize_map();
+        */
+
+      });
 
     },
 
@@ -29,7 +168,6 @@
       var _this = this;
     }
   };
-
   // Hooks the component into the app
   app.registerComponent( 'location-map', COMPONENT );
 } )( app );
--- components/location-map/location-map.php
+++ components/location-map/location-map.php
@@ -7,12 +7,15 @@
 */
 
 $defaults = [
+  'heading' => false,
+  'subheading' => false,
+  'map' => false
 ];
 
 $component_data = ll_parse_args( $component_data, $defaults );
-?>
 
-<?php
+if ( ll_empty( $component_data ) ) return;
+
 /**
  * Any additional classes to apply to the main component container.
  *
@@ -27,10 +30,70 @@ $classes        = $component_args['classes'] ?: array();
  * @var array
  * @see args['id']
  */
-$component_id   = $component_args['id'];
+$id               = ( $component_args['id'] ? $component_args['id'] : uniqid('location-hero-') );
+
+/**
+ * ACF values pulled into the component from the components.php partial.
+ */
+$heading          = $component_data['heading'];
+
+if( $heading ) {
+  $heading_txt = $heading['text'];
+  $heading_tag = $heading['tag'];
+}else{
+  $heading_txt = false;
+  $heading_tag = false;
+}
+
+$content          = $component_data['content'];
+$locations        = $component_data['locations'];
+$mid              = uniqid('location-hero-map-');
+$map              = '';
+
+foreach ( $locations as $location ) {
+  $map[] = get_field('location_map', $location->ID);
+}
 ?>
 
-<?php if ( ll_empty( $component_data ) ) return; ?>
-<div class="ll-location-map <?php echo implode( " ", $classes ); ?>" <?php echo ( $component_id ? 'id="'.$component_id.'"' : '' ) ?> data-component="location-map">
+<section class="ll-location-map relative<?php echo implode( " ", $classes ); ?>" <?php echo ( $id ? 'id="'.$id.'"' : '' ) ?> data-component="location-map">
 
-</div>
+  <div class="container row centered">
+
+    <div class="location-map__body col col-md-6of12 col-lg-6of12 col-xl-6of12 col-xxl-5of12">
+    <?php if( $heading_txt ) : ?>
+      <<?php echo $heading_tag; ?> class="location-map__heading">
+      <?php echo $heading_txt; ?>
+      </<?php echo $heading_tag; ?>>
+      <!-- .location-map__heading -->
+    <?php endif; ?>
+
+    <?php if ( $content || $locations ) : ?>
+      <div class="location-map__content">
+        <?php echo format_text($content); ?>
+
+        <?php if( $locations) : ?>
+
+          <ul class="location-map__locations no-bullet">
+
+          <?php foreach ( $locations as $location ) : ?>
+            <li>
+              <a href="<?php echo $location->guid; ?>"><?php echo $location->post_title; ?></a>
+            </li>
+          <?php endforeach; ?>
+
+          </ul>
+
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+    </div><!-- .col.col-md-6of12.col-lg-6of12.col-xl-6of12.col-xxl-6of12 -->
+
+    <div class="location-map__spacer col col-md-6of12 col-lg-6of12 col-xl-6of12 col-xxl-6of12">
+    </div>
+    <!-- .location-map__spacer.col.col-md-6of12>col-lg-6of12.col-xl-6of12.col-xxl-6of12 -->
+
+  </div><!-- .container.row -->
+
+  <div class="location-map__googlemap col col-md-6of12 col-lg-6of12 col-xl-6of12 col-xxl-6of12" id="<?php echo $mid; ?>" data-locations='<?php echo json_encode( $map ); ?>'>
+  </div>
+</section>
