7219b613fc6dc86ca1d7fb3364017d0e9b39ab90
MarkW <mark@liftedlogic.com>
2018-07-24 15:37:38 -0500

    Form Template page styles

--- templates/contents/content-form.php
+++ templates/contents/content-form.php
@@ -1,45 +1,25 @@
 <?php
   $form_id = ( get_field('form_id') ? get_field('form_id') : 1 );
-  $supertitle = get_field('superheader');
 ?>
-<article <?php post_class('form-skin'); ?>>
+<article <?php post_class(); ?>>
 
-<?php if( $supertitle ) : ?>
+<?php if( is_plugin_active( 'gravityforms/gravityforms.php' ) ) : ?>
 
-  <header class="gform_heading">
-  <?php
-    ll_include_component(
-      'supertitle',
-      array(
-        'text' => $supertitle
-      ),
-      array(
-        'classes' => [ 'gform_supertitle']
-      )
-    );
-  ?>
-    <h2 class="gform_title"><?php the_title(); ?></h2>
-
-    <?php the_content(); ?>
-
-  </header><!-- .supertitle -->
+  <?php gravity_form( $form_id, true, true ); ?>
 
 <?php endif; ?>
 
 <?php
+  $locations = array(
+    'heading'       => get_field('location_grid_heading'),
+    'content'       => get_field('location_grid_content'),
+    'num_locations' => get_field('location_grid_num_locations')
+  );
+
   ll_include_component(
     'location-grid',
-    array(
-      'num_locations' => get_field('location_grid_num_locations')
-    )
+    $locations
   );
 ?>
-
-<?php if( is_plugin_active( 'gravityforms/gravityforms.php' ) ) : ?>
-
-  <?php gravity_form( $form_id, false, false ); ?>
-
-<?php endif; ?>
-
-</article>
 <!-- .form-skin -->
+</article>
