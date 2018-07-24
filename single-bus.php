4cf4d66e7a46c44e1b5bc4eb21e2180f5d66a015
MarkW <mark@liftedlogic.com>
2018-07-24 08:25:58 -0500

    WIP Single New Bus Icon styles for the prefooter (all icons usable now) Cleanup

---
+++ single-bus.php
@@ -0,0 +1,6 @@
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/partials/header', 'page'); ?>
+  <?php include( locate_template('templates/partials/synopsis.php') ); ?>
+  <?php include( locate_template('templates/partials/components.php') ); ?>
+  <?php include( locate_template('templates/partials/prefooter.php') ); ?>
+<?php endwhile; ?>
