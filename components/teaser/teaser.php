f084de7c01e06f087dd69e4b857c8cabac55cea6
MarkW <mark@liftedlogic.com>
2018-07-26 12:23:38 -0500

    Added the "Learn More" button as an ACF for teasers

--- components/teaser/teaser.php
+++ components/teaser/teaser.php
@@ -9,6 +9,7 @@
 $defaults = [
   'heading'     => false,
   'content'     => false,
+  'button'      => false,
   'columns'     => false
 ];
 
@@ -41,6 +42,7 @@ $id            = ' id="' . $component_args['id'] . '"';
  */
 $heading     = $component_data['heading'];
 $content     = $component_data['content'];
+$button      = $component_data['button'];
 $columns     = $component_data['columns'];
 ?>
 
@@ -49,18 +51,15 @@ $columns     = $component_data['columns'];
 
   <div class="container">
 
-    <div class="row teaser__wrapper start">
+    <div class="row teaser__wrapper start centered">
 
-      <div class="teaser__heading col col-md-10of12 col-lg-9of12 col-xl-9of12 col-xxl-9of12">
+      <div class="teaser__heading col col-sm-8of12 col-md-9of12 col-lg-9of12 col-xl-9of12 col-xxl-9of12">
 
       <?php if( $heading ) : ?>
         <h2 class="teaser__headline"><?php echo $heading; ?></h2>
         <!-- .teaser__headline -->
       <?php endif; ?>
 
-      </div>
-
-      <div class="teaser__heading col col-md-10of12 col-lg-8of12 col-xl-8of12 col-xxl-8of12">
       <?php if( $content ) : ?>
 
         <div class="teaser__description">
@@ -68,43 +67,52 @@ $columns     = $component_data['columns'];
         </div><!-- .teaser__description -->
 
       <?php endif; ?>
+
       </div>
-      <!-- .teaser__heading.col.col-md-10of12.col-lg-8of12.col-xl-8of12.col-xxl-8of12 -->
 
-      <div class="teaser__body row">
+      <?php if( $button ) : ?>
 
-      <?php if( $columns ) : ?>
-
-        <?php foreach( $columns as $column ) : ?>
-          <div class="teaser__col col col-md-6of12 col-lg-4of12 col-xl-4of12 col-xxl-4of12">
-
-          <?php if( $column['icon'] ) : ?>
-            <div class="teaser__col__icon">
-              <svg class="icon <?php echo $column['icon']; ?>">
-                <use xlink:href="#<?php echo $column['icon']; ?>"></use>
-              </svg>
-            </div>
-          <?php endif; ?>
-
-          <?php if( $column['title'] ) : ?>
-            <h4 class="teaser__col__title"><?php echo $column['title']; ?></h4>
-            <!-- .teaser__col__title -->
-          <?php endif; ?>
-
-          <?php if( $column['caption'] ) : ?>
-            <div class="teaser__col__caption">
-            <?php echo format_text($column['caption']); ?>
-            </div><!-- .teaser__col__caption -->
-          <?php endif; ?>
-
-          </div>
-        <?php endforeach; ?>
+      <div class="teaser__button col col-sm-4of12 col-md-3of12 col-lg-3of12 col-xl-3of12 col-xxl-3of12 text-center">
+        <a class="btn" href="<?php echo $button['url'];?>"><?php echo $button['title'];?></a>
+      </div>
+      <!-- .teaser__button.col.col-md-10of12.col-lg-8of12.col-xl-8of12.col-xxl-8of12 -->
 
       <?php endif; ?>
 
-      </div><!-- .teaser__body.col.col-md-10of12.col-lg-10of12.col-xl-10of12.col-xxl-10of12 -->
-
     </div><!-- .row.teaser__wrapper -->
+
+    <div class="teaser__body row">
+
+    <?php if( $columns ) : ?>
+
+      <?php foreach( $columns as $column ) : ?>
+        <div class="teaser__col col col-md-6of12 col-lg-4of12 col-xl-4of12 col-xxl-4of12">
+
+        <?php if( $column['icon'] ) : ?>
+          <div class="teaser__col__icon">
+            <svg class="icon <?php echo $column['icon']; ?>">
+              <use xlink:href="#<?php echo $column['icon']; ?>"></use>
+            </svg>
+          </div>
+        <?php endif; ?>
+
+        <?php if( $column['title'] ) : ?>
+          <h4 class="teaser__col__title"><?php echo $column['title']; ?></h4>
+          <!-- .teaser__col__title -->
+        <?php endif; ?>
+
+        <?php if( $column['caption'] ) : ?>
+          <div class="teaser__col__caption">
+          <?php echo format_text($column['caption']); ?>
+          </div><!-- .teaser__col__caption -->
+        <?php endif; ?>
+
+        </div>
+      <?php endforeach; ?>
+
+    <?php endif; ?>
+
+    </div><!-- .teaser__body.col.col-md-10of12.col-lg-10of12.col-xl-10of12.col-xxl-10of12 -->
 
   </div><!-- .container -->
 
--- templates/partials/teaser.php
+++ templates/partials/teaser.php
@@ -2,7 +2,8 @@
 
   $teaser    = array(
     'heading'     => get_field('teaser_heading'),
-    'content'      => get_field('teaser_content'),
+    'content'     => get_field('teaser_content'),
+    'button'      => get_field('teaser_button'),
     'columns'     => get_field('teaser_columns')
   );
 
