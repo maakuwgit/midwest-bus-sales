0a8f1fe126bb877aa6b8869446dfd4560fee023a
MarkW <mark@liftedlogic.com>
2018-07-26 15:27:11 -0500

    404 page styles

--- 404.php
+++ 404.php
@@ -1,15 +1,20 @@
-<?php get_template_part('templates/partials/header', 'page'); ?>
+<header class="hdg">
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
-</div>
+  <div class="container">
 
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'roots'); ?></li>
-  <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
+    <h1 class="hdg__supertitle col col-md-8of12 col-lg-8of12 col-xl-8of12 col-xxl-8of12 end">Whoops!, We've found nothing here</h1><!-- .hdg__supertitle.col -->
 
-<?php get_search_form(); ?>
+    <h2 class="hdg__title col col-md-8of12 col-lg-8of12 col-xl-8of12 col-xxl-8of12 end">
+      <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+    </h2><!-- .hdg__title.col -->
+
+    <div class="hdg__content col col-md-8of12 col-lg-6of12 col-xl-6of12 col-xxl-6of12">
+      <p><?php _e('It looks like this was the result of either a mistyped address <br>or an out-of-date link', 'roots'); ?></p>
+      <?php get_search_form(); ?>
+    </div><!-- .hdg__content -->
+
+  </div><!-- .container -->
+
+</header><!-- .hdg -->
 
 
--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'roots'); ?>
-  </div>
-  <?php get_search_form(); ?>
+  <article class="container row start alert alert-warning">
+    <h1><?php _e('Sorry, no results were found.', 'roots'); ?></h1>
+    <?php get_search_form(); ?>
+  </article>
 <?php endif; ?>
 
 <?php get_template_part('templates/contents/hero', 'blog'); ?>
-<?php
-/*Dev Note: we don't use this since the blog grid component iterates
-  through the blog posts anyway
-  while (have_posts()) : the_post();
-  endwhile;
-*/
-?>
+
 <?php get_template_part('templates/contents/content', 'blog'); ?>
 
 <?php if ($wp_query->max_num_pages > 1) : ?>
